74a339fa9257b761fe60bd5a0daf358c9a611a59
Pehssa <154289664+LuanSantanaPeca@users.noreply.github.com>
2026-06-22 11:03:43 -0300

    Update db.js

--- trabalhofinal/lib/db.js
+++ trabalhofinal/lib/db.js
@@ -1,10 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 const os = require('os');
+const { put, list } = require('@vercel/blob');
 
 const TMP_FILE = path.join(os.tmpdir(), 'bd-alunos.json');
 const SOURCE_FILE = path.join(__dirname, '..', 'json', 'bd.json');
+const BLOB_PATHNAME = 'bd-alunos.json';
 const seedData = require('../json/bd.json');
+
+const useBlob = Boolean(process.env.BLOB_READ_WRITE_TOKEN);
 
 function loadSeedData() {
     if (fs.existsSync(SOURCE_FILE)) {
@@ -14,22 +18,66 @@ function loadSeedData() {
     return JSON.parse(JSON.stringify(seedData));
 }
 
-function readDb() {
+async function readDbFromBlob() {
+    const { blobs } = await list({ prefix: BLOB_PATHNAME, limit: 1 });
+
+    if (blobs.length > 0) {
+        const response = await fetch(blobs[0].url);
+
+        if (!response.ok) {
+            throw new Error('Failed to read database from blob storage');
+        }
+
+        return response.json();
+    }
+
+    const data = loadSeedData();
+    await writeDbToBlob(data);
+    return data;
+}
+
+async function writeDbToBlob(data) {
+    await put(BLOB_PATHNAME, JSON.stringify(data, null, 2), {
+        access: 'public',
+        addRandomSuffix: false,
+        allowOverwrite: true,
+        contentType: 'application/json'
+    });
+}
+
+function readDbFromFs() {
     if (fs.existsSync(TMP_FILE)) {
         return JSON.parse(fs.readFileSync(TMP_FILE, 'utf8'));
     }
 
     const data = loadSeedData();
-    writeDb(data);
+    writeDbToFs(data);
     return data;
 }
 
-function writeDb(data) {
+function writeDbToFs(data) {
     fs.writeFileSync(TMP_FILE, JSON.stringify(data, null, 2));
 }
 
-function getAlunos() {
-    return readDb().alunos;
+async function readDb() {
+    if (useBlob) {
+        return readDbFromBlob();
+    }
+
+    return readDbFromFs();
+}
+
+async function writeDb(data) {
+    if (useBlob) {
+        return writeDbToBlob(data);
+    }
+
+    writeDbToFs(data);
+}
+
+async function getAlunos() {
+    const db = await readDb();
+    return db.alunos;
 }
 
 module.exports = {
