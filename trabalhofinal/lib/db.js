4b8c8d8b8a703534f80b8d1e4365226a5b3516c8
Pehssa <154289664+LuanSantanaPeca@users.noreply.github.com>
2026-06-22 10:57:16 -0300

    Update db.js

--- trabalhofinal/lib/db.js
+++ trabalhofinal/lib/db.js
@@ -1,16 +1,26 @@
 const fs = require('fs');
 const path = require('path');
+const os = require('os');
 
-const TMP_FILE = path.join('/tmp', 'bd-alunos.json');
-const SOURCE_FILE = path.join(process.cwd(), 'json', 'bd.json');
+const TMP_FILE = path.join(os.tmpdir(), 'bd-alunos.json');
+const SOURCE_FILE = path.join(__dirname, '..', 'json', 'bd.json');
+const seedData = require('../json/bd.json');
+
+function loadSeedData() {
+    if (fs.existsSync(SOURCE_FILE)) {
+        return JSON.parse(fs.readFileSync(SOURCE_FILE, 'utf8'));
+    }
+
+    return JSON.parse(JSON.stringify(seedData));
+}
 
 function readDb() {
     if (fs.existsSync(TMP_FILE)) {
         return JSON.parse(fs.readFileSync(TMP_FILE, 'utf8'));
     }
 
-    const data = JSON.parse(fs.readFileSync(SOURCE_FILE, 'utf8'));
-    fs.writeFileSync(TMP_FILE, JSON.stringify(data));
+    const data = loadSeedData();
+    writeDb(data);
     return data;
 }
 
